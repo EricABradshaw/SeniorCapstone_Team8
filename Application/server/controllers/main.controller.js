fbad96de8ab55823cb332fb3d3f04d12b138e739
EricABradshaw <97480227+EricABradshaw@users.noreply.github.com>
2023-11-29 19:24:48 -0500

    Send create stego data to Flask as pngs (#9)

--- Application/server/controllers/main.controller.js
+++ Application/server/controllers/main.controller.js
@@ -1,10 +1,16 @@
 /* Controller for serving React files for frontend */
 const path = require('path')
+let frontEndPath = path.join(__dirname, '../../client/')
 
 const mainController = {
   sendReactApp: (req, res) => {
     try {
-      res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1800");
+      res.setHeader("Access-Control-Allow-Headers", "content-type");
+      res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+      res.sendFile(path.join(__dirname, `${frontEndPath}/build/index.html`))
     } catch (err) {}
   }
 }
