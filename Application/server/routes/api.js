8c96b7fe037ab6949a6b4a58c77ea2d8ce73b8c7
Treyton <99840907+TreytonPerkins@users.noreply.github.com>
2023-11-29 19:39:47 -0500

    upload stego

--- Application/server/routes/api.js
+++ Application/server/routes/api.js
@@ -2,14 +2,11 @@
 /* This will include business logic and endpoints for database interactions */
 const express = require('express')
 /* Communication from Node -> Flask */
-const multer = require('multer');
+
 /* end */
 const controller = require('../controllers/api.controller')
 const router = express.Router()
 
-/* multer configuration */ 
-const storage = multer.memoryStorage()
-const upload = multer({storage: storage})
 
 /* handle the post request that includes our data */
 router.post('/create_stego_image', upload.fields([
