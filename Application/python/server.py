0d33d632fd8763e6ae08fb349c2dbef4830ff7b6
Jacob Ridenour <112583892+jacobRidenour@users.noreply.github.com>
2024-02-14 12:16:31 -0500

    Newmodel (#16)
    * initial commit updated readme
    
    * not working just for reference
    
    * linux setup testing
    
    * update readme, hiding w/ new model
    
    * creation working, extraction not
    
    * first drafts of hiding / extracting working
    
    * small script changes
    
    * small fix to setup-win.bat
    
    ---------
    
    Co-authored-by: EricABradshaw <97480227+EricABradshaw@users.noreply.github.com>

--- Application/python/server.py
+++ Application/python/server.py
@@ -1,27 +1,27 @@
 from flask import Flask, request, jsonify, Response, send_file
 from flask_cors import CORS
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-from SteGuz import deploy_hide_image_op, deploy_reveal_image_op, sess, saver, preprocess_image, verbose
+#from SteGuz import deploy_hide_image_op, deploy_reveal_image_op, sess, saver, preprocess_image, verbose
+#import NSteGuz
+from NSteGuz import StegoModel
 import os
+import glob
 import argparse
 import io
 import base64
 import json
 from serverUtils import *
-if not verbose:
-    import logging
-    logging.disable(logging.WARNING)
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow.compat.v1 as tf
-tf.enable_eager_execution()
-tf.compat.v1.enable_eager_execution()
-tf.config.run_functions_eagerly(True)
-if not verbose:
-    logging.getLogger('tensorflow').disabled = True
-    import warnings
-    warnings.filterwarnings('ignore')
+# if not verbose:
+#     import logging
+#     logging.disable(logging.WARNING)
+#     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+
+# if not verbose:
+#     logging.getLogger('tensorflow').disabled = True
+#     import warnings
+#     warnings.filterwarnings('ignore')
 
 Debug = True
 
@@ -29,103 +29,87 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost:9000"]}})
 
 
-# @app.route('/create_stego_image', methods=['POST'])
-# def create_stego_image():
-#   try:
-#     print("Flask request received")
-#     cover_image_string = request.json.get('coverString', '')
-#     secret_image_string = request.json.get('secretString', '')
+# @app.route('/calculate_metrics', methods=['POST'])
+# def calculate_metrics():
+#     if 'secretImage' not in request.files:
+#         return jsonify({"error": "Secret image must be provided"}), 400
     
-#     cover_image = base64_to_image(cover_image_string)
-#     secret_image = base64_to_image(secret_image_string)
+#     if not request.files.getlist('coverImages[]'):
+#         return jsonify({"error": "No cover images provided"}), 400
     
-#     print("Images received!")
-#     return 'Images received!', 200
-#   except Exception as e:
-#     print(f"Error: {str(e)}")
-#     return 'Error!', 500 
+#     secretImageFile = request.files['secretImage']
+#     secretImageString = io.BytesIO(secretImageFile.read())
+#     index = request.form.get('index', type=int, default=0)
+    
+#     secretImage = base64_to_image(secretImageString)
+    
+#     # Navigate to /Application/models/ and prepare models/folderIndex/ for loading
+#     cwd = os.path.dirname(os.path.abspath(__file__))
+#     modelsDir = os.path.join(os.path.dirname(cwd), 'models')
+#     modelPaths = get_model_paths(modelsDir)
+#     inputModelPath = modelPaths[index]
 
-
-@app.route('/calculate_metrics', methods=['POST'])
-def calculate_metrics():
-    if 'secretImage' not in request.files:
-        return jsonify({"error": "Secret image must be provided"}), 400
-    
-    if not request.files.getlist('coverImages[]'):
-        return jsonify({"error": "No cover images provided"}), 400
-    
-    secretImageFile = request.files['secretImage']
-    secretImageString = io.BytesIO(secretImageFile.read())
-    index = request.form.get('index', type=int, default=0)
-    
-    secretImage = base64_to_image(secretImageString)
-    
-    # Navigate to /Application/models/ and prepare models/folderIndex/ for loading
-    cwd = os.path.dirname(os.path.abspath(__file__))
-    modelsDir = os.path.join(os.path.dirname(cwd), 'models')
-    modelPaths = get_model_paths(modelsDir)
-    inputModelPath = modelPaths[index]
-
-    # Load the model here since the index being sent in may vary -
-    # each index corresponds to a different model.
-    metrics_list = []  
-    try:
-        saver.restore(sess, inputModelPath)
-        tf.train.load_checkpoint(inputModelPath)
+#     # Load the model here since the index being sent in may vary -
+#     # each index corresponds to a different model.
+#     metrics_list = []  
+#     try:
+#         saver.restore(sess, inputModelPath)
+#         tf.train.load_checkpoint(inputModelPath)
         
-        # Preprocess the secret image
-        secretImagePreproc = preprocess_image(secretImage)
+#         # Preprocess the secret image
+#         secretImagePreproc = preprocess_image(secretImage)
 
-        # for each provided cover image...
-        for coverImageFile in request.files.getlist('coverImage[]'):
-            metrics = []
+#         # for each provided cover image...
+#         for coverImageFile in request.files.getlist('coverImage[]'):
+#             metrics = []
             
-            coverImageString = io.BytesIO(coverImageFile.read())
-            coverImage = base64_to_image(coverImageString)
-            coverImagePreproc = preprocess_image(coverImage)
+#             coverImageString = io.BytesIO(coverImageFile.read())
+#             coverImage = base64_to_image(coverImageString)
+#             coverImagePreproc = preprocess_image(coverImage)
             
-            # Generate the Stego Image using the loaded model
-            stegoImage = sess.run(deploy_hide_image_op,
-                                feed_dict={"input_prep:0": [secretImagePreproc], "input_hide:0": [coverImagePreproc]})
+#             # Generate the Stego Image using the loaded model
+#             stegoImage = sess.run(deploy_hide_image_op,
+#                                 feed_dict={"input_prep:0": [secretImagePreproc], "input_hide:0": [coverImagePreproc]})
         
-            # Clean up the image
-            stegoImage = stegoImage.squeeze()
-            stegoImage = np.clip(stegoImage, 0, 1)
-            # stegoImage = (stegoImage * 255).astype(np.uint8)
+#             # Clean up the image
+#             stegoImage = stegoImage.squeeze()
+#             stegoImage = np.clip(stegoImage, 0, 1)
+#             # stegoImage = (stegoImage * 255).astype(np.uint8)
             
-            # we've hidden the image, now get the PSNR
-            # any additional preprocessing we need to do?
-            psnr = get_psnr(stegoImage, coverImagePreproc)
-            metrics.append(psnr)
+#             # we've hidden the image, now get the PSNR
+#             # any additional preprocessing we need to do?
+#             psnr = get_psnr(stegoImage, coverImagePreproc)
+#             metrics.append(psnr)
             
-            # extract the image -> get SSIM
+#             # extract the image -> get SSIM
             
-            # Extract the Hidden Image using the loaded model
-            extractedImage = sess.run(deploy_reveal_image_op,
-                                    feed_dict={"deploy_covered:0": stegoImage})
+#             # Extract the Hidden Image using the loaded model
+#             extractedImage = sess.run(deploy_reveal_image_op,
+#                                     feed_dict={"deploy_covered:0": stegoImage})
             
-            # Clean up the image
-            extractedImage = np.clip(extractedImage, 0, 1)
+#             # Clean up the image
+#             extractedImage = np.clip(extractedImage, 0, 1)
                     
-            # we've extracted the image, now get the SSIM
-            # any additional preprocessing we need to do?
-            ssim = get_ssim(extractedImage, secretImage)
-            metrics.append(ssim)
+#             # we've extracted the image, now get the SSIM
+#             # any additional preprocessing we need to do?
+#             ssim = get_ssim(extractedImage, secretImage)
+#             metrics.append(ssim)
             
-            metrics_list.append(metrics)
+#             metrics_list.append(metrics)
             
-        # Return the metrics list
-        return jsonify(metrics_list)
-    except Exception as e:
-        return jsonify({"error": "Model could not be loaded . Details: " + str(e)}), 500
+#         # Return the metrics list
+#         return jsonify(metrics_list)
+#     except Exception as e:
+#         return jsonify({"error": "Model could not be loaded . Details: " + str(e)}), 500
 
 
 @app.route('/extract_hidden_image', methods=['POST'])
 def extract_hidden_image():
-    # if 'stegoImage' not in request.files:
-    #     return jsonify({"error": "Stego image must be provided"}), 400
-
-    # stegoImageFile = request.files['stegoImage']
+    # # Check for beta value sent from front end; default to 0.5 if not provided
+    #beta = request.json.get('beta', 0.50)
+    beta = 0.50
+    print(f'BETA IS {beta}')
+    
     index = request.form.get('index', type=int, default=0)
     data = json.loads(request.data.decode('utf-8'))
     
@@ -133,36 +117,54 @@ def extract_hidden_image():
     stegoImage = np.expand_dims(stegoImage, axis=0)
     stegoImage = stegoImage[:, :, :, :3]
     stegoImage = stegoImage / 255.0
-    # stegoImage = preprocess_image(stegoImage)
-    # Navigate to /Application/models/ and prepare models/folderIndex/ for loading
+
+    # Assign/validate beta value
+    try:
+        beta = float(beta)
+        if not (0 <= beta <= 1):
+            raise ValueError("Beta value must be between 0 and 1.")
+    except ValueError as e:
+        return jsonify({"error": "Invalid beta value. Details: " + str(e)}), 400
+    print(f'BETA IS {beta}')
+
+    # # Load the appropriate model based on the provided beta value
+    targetBetas = [0.25, 0.50, 0.75]
+    closestBeta = min(targetBetas, key=lambda x: abs(x - beta))
+    print(f'CLOSEST BETA IS {closestBeta}')
+
+    # # Navigate to /Application/models/ and prepare models/folderIndex/ for loading
     cwd = os.path.dirname(os.path.abspath(__file__))
     modelsDir = os.path.join(os.path.dirname(cwd), 'models')
-    modelPaths = get_model_paths(modelsDir)
-    inputModelPath = modelPaths[index]
-    # Load the model here since the index being sent in may vary -
-    # each index corresponds to a different model.
+    betaFolderName = f"b{closestBeta:.2f}"
+    print(f'Attempting to load model with {betaFolderName}')
+    
+    modelFolder = glob.glob(os.path.join(modelsDir, betaFolderName + "*"))
+    if not modelFolder:
+        return jsonify({"error": f"No model found for beta value: {beta}"}), 404
+    #print(f'MODEL FOLDER SELECTED IS {modelFolder}')
+    inputModelPath = modelFolder[0]
+
+    # Load the model
     try:
-        with sess.graph.as_default():
-          saver.restore(sess, inputModelPath)
-        tf.train.load_checkpoint(inputModelPath)
-
-        # Extract the Hidden Image using the loaded model
-        extractedImage = sess.run(deploy_reveal_image_op,
-                                feed_dict={"deploy_covered:0": stegoImage})
-
-        # Limit the values to between 0 and 1
-        # Clean up the image so it's a proper PNG
-        extractedImage = extractedImage.squeeze()
+        #model = tf.keras.models.load_model(inputModelPath)
+        model = StegoModel()
+        model.load_weights(inputModelPath)
+        
+        # How we generated the Stego Image using the loaded model
+        extractedImage = model.extract((stegoImage))
+               
+        # Limit the values to between 0 and 1; clean up the image so it's a proper PNG
+        extractedImage = extractedImage.numpy().squeeze()
         extractedImage = np.clip(extractedImage, 0, 1)
         extractedImage = (extractedImage * 255).astype(np.uint8)
-
+        
         # Now convert it into a byte stream
         extractedImageByteArray = io.BytesIO()
         Image.fromarray(extractedImage).save(extractedImageByteArray, format='PNG')
         extractedImageByteArray = extractedImageByteArray.getvalue()
         extractedImageBase64 = base64.b64encode(extractedImageByteArray).decode('utf-8')
-        # Return the stego image
-        # return Response(response=stegoImage, mimetype='image/png')
+        
+        # Return the extracted image
         return jsonify({"message":"Success", "hiddenImage":extractedImageBase64}), 200
     except Exception as e:
         print(str(e))
@@ -171,65 +173,86 @@ def extract_hidden_image():
 
 @app.route('/create_stego_image_b64', methods=['POST'])
 def create_stego_image():
-    tf.enable_eager_execution()
-    tf.compat.v1.enable_eager_execution()
-    tf.config.run_functions_eagerly(True)
-    data = request.json.get('data', {})
-    if data:
-      coverImageString = data.get('coverString', '')
-      secretImageString = data.get('secretString', '')
-
-    model = request.json.get('model', '')
+    coverImageString = request.json.get('coverString', '')
+    secretImageString = request.json.get('secretString', '')
+    # Check for beta value sent from front end; default to 0.5 if not provided
+    beta = request.json.get('beta', 0.50)
+    print(f'BETA IS {beta}')
+    
     if Debug:
         print("Flask request received.")
         
     if model:
       print(f'Selected model: {model}')
     
+    # Get cover/secret image
     if not coverImageString:
         return 'Error! No cover image provided', 500 
         
     if not secretImageString:
         return 'Error! No secret image provided', 500 
     
-    index = 0
-    #index = request.form.get('index', type=int, default=0)
-    
-    #coverImageString = io.BytesIO(coverImageFile.read())
-    #secretImageString = io.BytesIO(secretImageFile.read())
-    #coverImage.show()
-    #secretImage.show()
-    
     coverImage = base64_to_image(coverImageString)
     secretImage = base64_to_image(secretImageString)
     
+    # Assign/validate beta value
+    try:
+        beta = float(beta)
+        if not (0 <= beta <= 1):
+            raise ValueError("Beta value must be between 0 and 1.")
+    except ValueError as e:
+        return jsonify({"error": "Invalid beta value. Details: " + str(e)}), 400
+    #print(f'BETA IS {beta}')
+
+    # Load the appropriate model based on the provided beta value
+    targetBetas = [0.25, 0.50, 0.75]
+    closestBeta = min(targetBetas, key=lambda x: abs(x - beta))
+    #print(f'CLOSEST BETA IS {closestBeta}')
+
     cwd = os.path.dirname(os.path.abspath(__file__))
     modelsDir = os.path.join(os.path.dirname(cwd), 'models')
-    modelPaths = get_model_paths(modelsDir)
-    inputModelPath = modelPaths[index]
-    # Load the model here since the index being sent in may vary -
-    # each index corresponds to a different model.
+    betaFolderName = f"b{closestBeta:.2f}"
+    print(f'Attempting to load model with {betaFolderName}')
+    
+    modelFolder = glob.glob(os.path.join(modelsDir, betaFolderName + "*"))
+    if not modelFolder:
+        return jsonify({"error": f"No model found for beta value: {beta}"}), 404
+    #print(f'MODEL FOLDER SELECTED IS {modelFolder}')
+    inputModelPath = modelFolder[0]
+    
+    print(f'Attempting to load model from {inputModelPath}')
+    # Load the model
     try:
-        with sess.graph.as_default():
-          saver.restore(sess, inputModelPath)
-        tf.train.load_checkpoint(inputModelPath)
-        # Preprocess the images
-        coverImagePreproc = preprocess_image(coverImage)
-        secretImagePreproc = preprocess_image(secretImage)
+        #model = tf.keras.models.load_model(inputModelPath)
+        model = StegoModel()
+        model.load_weights(inputModelPath)
+        
+        # Preprocess the images 
+        coverImagePreproc = preprocess_image(coverImage).astype(np.float32)
+        secretImagePreproc = preprocess_image(secretImage).astype(np.float32)
+        
         # Generate the Stego Image using the loaded model
-        stegoImage = sess.run(deploy_hide_image_op,
-                            feed_dict={"input_prep:0": [secretImagePreproc], "input_hide:0": [coverImagePreproc]})
+        # _, stegoImage = model.call((
+        #     np.expand_dims(secretImagePreproc, axis=0),
+        #     np.expand_dims(coverImagePreproc, axis=0)
+        # ))
+        stegoImage = model.hide((
+            np.expand_dims(secretImagePreproc, axis=0),
+            np.expand_dims(coverImagePreproc, axis=0)
+        ))
+        
         # Clean up the image so it's a proper PNG
-        stegoImage = stegoImage.squeeze()
+        stegoImage = stegoImage.numpy().squeeze()
         stegoImage = np.clip(stegoImage, 0, 1)
         stegoImage = (stegoImage * 255).astype(np.uint8)
-        # Now convert it into a byte stream
+        
+        # Now convert it into a byte stream then base64
         stegoImageByteArray = io.BytesIO()
         Image.fromarray(stegoImage).save(stegoImageByteArray, format='PNG')
         stegoImageByteArray = stegoImageByteArray.getvalue()
         stegoImageBase64 = base64.b64encode(stegoImageByteArray).decode('utf-8')
+        
         # Return the stego image
-        # return Response(response=stegoImage, mimetype='image/png')
         return jsonify({"message":"Success", "stegoImage":stegoImageBase64}), 200
     except Exception as e:
         return jsonify({"error": "Model could not be loaded . Details: " + str(e)}), 500
--- README.md
+++ README.md
@@ -1,40 +1,93 @@
-# SeniorCapstone_Team8
+# StegoSource
 
-Senior Capstone project Team 8 AY 2023-24
+Purdue Fort Wayne Senior Capstone Project AY 2023-24, Team 8
 
-## Python Setup
+Utilizing a CNN-Based Image-in-Image Steganography Model in a Web-Based Application
 
-The models were developed using an older version of TensorFlow; 1.xx compatibility has decreased in later releases and full-blown conversion to 2.xx is not within our scope, so to properly use the models we need a version of Python that is compatible with TensorFlow 2.8.0, the latest of which is Python 3.10.11.
+This project utilizes a CNN-based machine learning model developed by Dr. Amal Khalifa and her team at Purdue University Fort Wayne. 
 
-While not strictly required it's probably best to uninstall any other versions of Python.
+## Dependency Installation
 
-Install [Python 3.10.11](https://www.python.org/downloads/release/python-31011/)
+### Python
 
+The image-in-image steganography models used for this project are CNN models utilizing the SteGuz method. You will need to install a version of Python that supports TensorFlow version 2.12.0.
+
+We have been developing and running this application under Python [Python 3.10.11](https://www.python.org/downloads/release/python-31011/).
+
+*While not strictly required it is best to have only one version of Python installed on your machine.*
+
+While installing Python:
 - Be sure to check the box that says "Add Python 3.x to PATH"
-- After installation, add `%appdata%\Python\Python3x\Scripts` to your PATH.
+- After installation, add `%appdata%\Python\Python3x\Scripts` to PATH (if it's not already there).
 
-## Development Setup Instructions
+### node.js
 
+node.js gives us the package manager `npm` and is also the main back-end for our application. `npm` is also necessary to install the required front-end packages, namely React and its dependencies.
+
+Download and install it from [here](https://nodejs.org/en/download/current).
+
+### MongoDB
+
+MongoDB is required for the database utilized by this web application. Download and install it from [here](https://www.mongodb.com/try/download/community).
+
+## Quick Setup
+
+Install the dependencies as described above.
+
+If you are running Bash on a Windows machine:
+```
+git clone https://github.com/EricABradshaw/SeniorCapstone_Team8.git
+cd SeniorCapstone_Team8
+./setup-Win.sh
+cd Application
+npm run all
+```
+
+If you are running Bash on a Linux machine:
+```
+git clone https://github.com/EricABradshaw/SeniorCapstone_Team8.git
+cd SeniorCapstone_Team8
+./setup-Linux.sh
+cd Application
+npm run all
+```
+
+The server should automatically run in the Python virtual environment. Your terminal does not need to be in the virtual environment when calling `npm run all`.
+
+## Manual Setup
+
+### Clone this repository
+
+In any terminal:
 ```
 git clone https://github.com/EricABradshaw/SeniorCapstone_Team8.git
 cd SeniorCapstone_Team8/Application/python
+```
+
+### Activate the virtual environment
+
+If you are on Windows using Bash: 
+```
 py -3 -m venv .venv
-```
-
-If you are using Bash or an equivalent shell:
-
-```
 . .venv/Scripts/activate
 ```
 
-If you are using command prompt or PowerShell:
-
+If you are on Linux using Bash or equivalent:
 ```
+python3 -m venv .venv
+source ./.venv/bin/activate
+```
+
+If you are on Windows using PowerShell or Command Prompt:
+```
+py -3 -m venv .venv
 .venv\Scripts\activate
 ```
+You will know you're in the virtual environment when all commands output by your shell end with `(.venv)`
 
-In VS Code, open the command palette (Ctrl+Shift+P) and type "Python: select interpreter...". Set it to the python.exe found in your .venv folder.
+### Install npm Packages and Python Dependencies
 
+In any terminal with the Python virtual environment active:
 ```
 pip install -r requirements.txt
 cd ..
@@ -45,17 +98,16 @@ npm i -g concurrently
 npm i
 ```
 
-The above setup can be done via the setup shell script from either
+You will also need to create a file called `.env` in `/Application/` with the following contents:
+```
+PORT=9000
+MONGO_URI="mongodb://127.0.0.1/<databasename>"
+FLASK_SERVER_URI="localhost:5000"
+```
 
-- Bash or Bash-like:
-  `source setup.sh`
-- PowerShell:
-  `.\setup.bat`
+### Start the web app locally
 
-These scripts will also create a .env file for you in /Application/.
-
-To run the environment:
-
+Finally, to start the web server on your local machine:
 ```
 npm run all
 ```
