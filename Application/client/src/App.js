b01b7f198c611e09fb41aee3f08727bee8577869
EricABradshaw <97480227+EricABradshaw@users.noreply.github.com>
2024-03-11 10:35:29 -0400

    Trying to fix routing (#27)
    * Fixed navigation so that it uses buttons
    Added some bootstrap to get started
    
    * Responsive nav bar
    
    * Fixed sizing with screen

--- Application/client/src/App.js
+++ Application/client/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState} from 'react'
+import { Button, Dropdown } from 'react-bootstrap'
+
 import './index.scss'
+import './bsOverride.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // import components
-import ToggleButton from './ToggleButton';
 import Hide from './Hide';
 import Extract from './Extract';
 import About from './About'
@@ -10,42 +13,51 @@ import HideText from './HideText'
 import Recommend from './Recommend';
 
 function App() {
-  const [isComponentAVisible, setComponentAVisible] = useState(true);
   const [currentPage, setCurrentPage] = useState('main')
+  const [activeButton, setActiveButton] = useState('main')
 
   const navigate = (page) => {
+    setActiveButton(page)
     setCurrentPage(page)
   }
 
   let pageContent
   if (currentPage === 'about') {
     pageContent = <About />
-  } else if (currentPage === 'main') {
-    pageContent = isComponentAVisible ? <Hide /> : <Extract />
   } else if (currentPage === 'text') {
     pageContent = <HideText />
   } else if (currentPage === 'recommend') {
     pageContent = <Recommend />
+  } else if (currentPage === 'extract') {
+    pageContent = <Extract />
+  } else {
+    pageContent = <Hide />
   }
 
-  //Callback function to decide which Elements are visible either hiding or extraction
-  const handleToggle = () => {
-    setComponentAVisible(!isComponentAVisible);
-  };
-
   return (
-    <div className="App">
-      <span id="app-header">
-        <h1>StegoSource.net</h1>
-        <button onClick={() => navigate('about')}>About</button>
-        <button onClick={()=> navigate('recommend')}>Recommend Page</button>
-        {currentPage === 'main' ? (
-          <button onClick={() => navigate('text')}>Hide Text</button>
-        ) : (
-          <button onClick={() => navigate('main')}>Hide Image</button>
-        )}
-        <ToggleButton onToggle={handleToggle}/>
-      </span>
+    <div className="App bg-dark container-fluid p-0" style={{height:'100%', minHeight:'100vh', overflow:'hidden'}}>
+      <div className='mx-auto p-2 row w-100 bg-black border-bottom'> 
+        <h1 id='title' className='w-25 py-3 px-5'>StegoSource.net</h1>
+        <Dropdown className="col-12 d-block d-sm-none">
+          <Dropdown.Toggle variant="dark" id="dropdown-basic">
+            Menu
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => navigate('about')}>About</Dropdown.Item>
+            <Dropdown.Item onClick={() => navigate('main')}>Hide Images</Dropdown.Item>
+            <Dropdown.Item onClick={() => navigate('text')}>Hide Text</Dropdown.Item>
+            <Dropdown.Item onClick={() => navigate('extract')}>Extract</Dropdown.Item>
+            <Dropdown.Item onClick={() => navigate('recommend')}>Recommend StegoImages</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <div className='d-none d-sm-flex w-100 justify-content-around row m-auto'>
+          <Button id='aboutBtn' className={`col-sm-6 col-md-3 col-lg-2 custom-button ${activeButton === "about" ? 'active' : ''}`} onClick={() => navigate('about')}>About</Button>
+          <Button id='hideBtn' className={`col-sm-6 col-md-3 col-lg-2 custom-button ${activeButton === "main" ? 'active' : ''}`} onClick={() => navigate('main')}>Hide Images</Button>
+          <Button id='textBtn' className={`col-sm-6 col-md-3 col-lg-2 custom-button ${activeButton === "text" ? 'active' : ''}`} onClick={() => navigate('text')}>Hide Text</Button>
+          <Button id='extractBtn' className={`col-sm-6 col-md-3 col-lg-2 custom-button ${activeButton === "extract" ? 'active' : ''}`} onClick={() => navigate('extract')}>Extract</Button>
+          <Button id='recommendBtn' className={`col-sm-6 col-md-6 col-lg-2 custom-button ${activeButton === "recommend" ? 'active' : ''}`} onClick={() => navigate('recommend')}>Recommend</Button>
+        </div>
+      </div>
       <div>
         {pageContent}
       </div>
